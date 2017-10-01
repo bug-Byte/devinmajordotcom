a075f79180f3e1bfb0b05e35042634892d861857
Devin Major <dmajor@ssmic.com>
2017-10-01 12:07:02 -0400

    project initialization

---
+++ devinmajordotcom/Controllers/PortfolioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace devinmajordotcom.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title="D3V!N M@J0R";
+            return View();
+        }
+    }
+}
---
+++ devinmajordotcom/Scripts/IndexScripts.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    $(".work-wrapper").hover(function () {
+        $(this).find('.glyphicon').addClass('blueGlyphicon', 600);
+    }, function () {
+        $(this).find('.glyphicon').removeClass('blueGlyphicon', 600);
+    });
+
+    $(".masthead-nav").find('li').each(function () {
+        $(this).click(function () {
+            $('.masthead-nav li').removeClass('active');
+            var divToShow = $(this).data('activediv');
+            $(this).addClass('active');
+            $(".inner.cover").hide();
+            $(this).show();
+            $(divToShow).fadeIn(500);
+        });
+    });
+
+});
