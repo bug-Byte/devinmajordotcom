5f3833cf8a2ce7d0f6a39a770cb7847441992ac0
Devin Major <dmajor@ssmic.com>
2017-10-09 17:57:29 -0400

    media dashboard is started

---
+++ devinmajordotcom/Scripts/mediaScripts.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    $(document).on("click", ".menu li a", function () {
+
+        $(".menu li a").removeClass('active');
+
+        $(this).addClass('active');
+
+    });
+
+});
