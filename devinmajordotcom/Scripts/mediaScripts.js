fa9950cbf05817472516146007c999de1db05b4c
Devin Major <dmajor@ssmic.com>
2017-10-16 14:51:46 -0400

    got media dashboard sidebar running

--- devinmajordotcom/Scripts/mediaScripts.js
+++ devinmajordotcom/Scripts/mediaScripts.js
@@ -1,12 +1,24 @@
-﻿$(document).ready(function () {
+﻿var newWidth = $('.site-wrapper').width() - 74.67;
+
+$(document).ready(function () {
 
     $(document).on("click", ".menu li a", function () {
         $(".menu li a").removeClass('active');
         $(this).addClass('active');
     });
 
-    $(document).on("hover", "#menu", function() {
-        $("#menu").toggleClass("toggle");
+    $('#nav1').bind("mouseenter", function () {
+        $('#site-wrapper').animate({ left: '74.67px', width: newWidth }, 500);
+        $('#nav').animate({ width: '5em' }, 500);
     });
 
+    $('#nav').bind("mouseleave", function () {
+        $('#site-wrapper').animate({ left: '0px', width: '100%' }, 500);
+        $('#nav').animate({ width: '0px' }, 500);
+    });
+
+});
+
+$(window).on("resize", function () {
+    newWidth = $('.site-wrapper').width() - 74.67;
 });
