0a44c5c035a7304184c91311f1d4fc6f73c9b36e
Devin Major <dmajor@ssmic.com>
2017-12-10 10:45:16 -0500

    custom error pages

---
+++ devinmajordotcom/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace devinmajordotcom.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HandleError]
+        public ActionResult Error(HandleErrorInfo error)
+        {
+            return View(error);
+        }
+
+        [HandleError]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
