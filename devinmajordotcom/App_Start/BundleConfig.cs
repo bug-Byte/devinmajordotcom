732b2c067c4a4418c9a6355557cb2b11ff5dde9c
CGC\dmajor <dmajor@ssmic.com>
2018-02-26 13:42:02 -0500

    got a good start on homepage

--- devinmajordotcom/App_Start/BundleConfig.cs
+++ devinmajordotcom/App_Start/BundleConfig.cs
@@ -12,51 +12,59 @@ namespace devinmajordotcom.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui-{version}.js",
-                        "~/Scripts/jquery.unobtrusive-ajax.js"
-                        ));
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-ui-{version}.js",
+                "~/Scripts/jquery.unobtrusive-ajax.js"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*",
-                        "~/Scripts/jquery.validate.unobtrusive.js"
-                        ));
+                "~/Scripts/jquery.validate*",
+                "~/Scripts/jquery.validate.unobtrusive.js"
+            ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"
-                        ));
+                "~/Scripts/modernizr-*"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"
-                      ));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"
+            ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/cover.css",
-                      "~/Content/bootsnack.css",
-                      "~/Content/font-awesome.css",
-                      "~/Content/site.css",
-                      "~/Content/bootstrap-iconpicker.min.css"
-                      ));
+                "~/Content/bootstrap.css",
+                "~/Content/cover.css",
+                "~/Content/bootsnack.css",
+                "~/Content/font-awesome.css",
+                "~/Content/site.css",
+                "~/Content/bootstrap-iconpicker.min.css"
+            ));
 
             bundles.Add(new StyleBundle("~/Content/mediacss").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/bootstrap-theme.css",
-                      "~/Content/cover2.css",
-                      "~/Content/font-awesome.css"
-                      ));
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css",
+                "~/Content/cover2.css",
+                "~/Content/font-awesome.css"
+            ));
+
+            bundles.Add(new StyleBundle("~/Content/homecss").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css",
+                "~/Content/magnific-popup.css",
+                "~/Content/font-awesome.css",
+                "~/Content/templatemo-style2.css"
+            ));
 
             bundles.Add(new StyleBundle("~/Content/portfoliocss").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/bootstrap-theme.css",
-                      "~/Content/font-awesome.css",
-                      "~/Content/reset.css",
-                      "~/Content/style.css",
-                      "~/Content/templatemo-style.css"
-                      ));
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css",
+                "~/Content/font-awesome.css",
+                "~/Content/reset.css",
+                "~/Content/style.css",
+                "~/Content/templatemo-style.css"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/customScripts").Include(
                 "~/Scripts/jquery.bootsnack.js",
@@ -66,7 +74,7 @@ namespace devinmajordotcom.App_Start
                 "~/Scripts/jquery.signalR-2.2.2.min.js",
                 "~/Scripts/jquery.easypiechart.min.js",
                 "~/Scripts/IndexScripts.js"
-                    ));
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/tinymce").Include(
                 "~/Scripts/tinymce/tinymce.min.js",
@@ -74,7 +82,7 @@ namespace devinmajordotcom.App_Start
                 "~/Scripts/tinymce/themes/inlite/theme.min.js",
                 "~/Scripts/tinymce/themes/mobile/theme.min.js",
                 "~/Scripts/tinymce/themes/modern/theme.min.js"
-                    ));
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/portfolioScripts").Include(
                 "~/Scripts/smoothscroll.js",
@@ -83,14 +91,21 @@ namespace devinmajordotcom.App_Start
                 "~/Scripts/custom.js",
                 "~/Scripts/index.js",
                 "~/Scripts/imagesloaded.min.js"
-                      ));
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/mediaScripts").Include(
                 "~/Scripts/smoothscroll.js",
                 "~/Scripts/jquery.nav.js",
                 "~/Scripts/isotope.js",
                 "~/Scripts/mediaScripts.js"
-                      ));
+            ));
+
+            bundles.Add(new ScriptBundle("~/bundles/homeScripts").Include(
+                "~/Scripts/jquery.stellar.min.js",
+                "~/Scripts/jquery.magnific-popup.min.js",
+                "~/Scripts/smoothscroll.js",
+                "~/Scripts/homeScripts.js"
+            ));
 
         }
     }
