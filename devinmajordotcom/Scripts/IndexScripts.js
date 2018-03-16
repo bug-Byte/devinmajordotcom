89316c337e29046b679da8dc459b0bbab462f9b2
Devin Major <dmajor@ssmic.com>
2018-03-15 22:21:56 -0400

    fix for IE

--- devinmajordotcom/Scripts/IndexScripts.js
+++ devinmajordotcom/Scripts/IndexScripts.js
@@ -90,10 +90,31 @@ function draw() {
     }
 }
 
+$(window).bind("resize", function () {
+    var w = $(window).width();
+    var h = $(window).height();
+    $("#particles").width(w + "px");
+    $("#particles").height(h + "px");
+});
+
+
 $(document).ready(function () {
 
     // Start
     loop();
+
+    var ms_ie = false;
+    var ua = window.navigator.userAgent;
+    var old_ie = ua.indexOf('MSIE ');
+    var new_ie = ua.indexOf('Trident/');
+
+    if ((old_ie > -1) || (new_ie > -1)) {
+        ms_ie = true;
+    }
+
+    if (!ms_ie) {
+        $("#particles").show();
+    }
 
     InitializePieCharts();
     
