01b1d88e225810318b30a1a53b6035a84f74de23
CGC\dmajor <dmajor@ssmic.com>
2018-03-21 14:15:15 -0400

    fixed popover bug, need to handle sgn up success

--- devinmajordotcom/Scripts/homeScripts.js
+++ devinmajordotcom/Scripts/homeScripts.js
@@ -64,20 +64,6 @@ $(document).ready(function () {
         }
     });
 
-    $('body').on('click', function (e) {
-        $('[data-toggle=popover]').each(function () {
-            // hide any open popovers when the anywhere else in the body is clicked
-            if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
-                $(this).popover('toggle');
-            }
-        });
-    });
-
-    //$("[data-toggle=popover]").mousedown(function () {
-    //    // toggle popover when link is clicked
-    //    $(this).popover('toggle');
-    //});
-
     // MENU
     $('.navbar-collapse a').on('click',function(){
       $(".navbar-collapse").collapse('hide');
